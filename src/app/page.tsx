523959f6d16b1918886f7e48b3b96fffdd31d585
svirins <svirins@gmail.com>
2024-01-19 23:04:34 +0300

    feat: prisma schema 4 post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import { unstable_noStore as noStore } from "next/cache";
 import { api } from "@/trpc/server";
-import { UserButton } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, currentUser } from "@clerk/nextjs";
 
 export default async function Home() {
   noStore();
+  const user = await currentUser();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <UserButton afterSignOutUrl="/" />
+        {!user && <SignInButton />}
+        {!!user && <SignOutButton />}
       </div>
     </main>
   );
