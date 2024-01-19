b2199102ad9606ac38cc7943ce6fbd0ec16b01c5
svirins <svirins@gmail.com>
2024-01-20 00:04:45 +0300

    chore: stash

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,11 +14,10 @@ const config = {
     // These opinionated rules are enabled in stylistic-type-checked above.
     // Feel free to reconfigure them to your own preference.
     "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/no-unsafe-call": "warn",
-    "@typescript-eslint/no-unsafe-assignment": "warn",
-
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
-
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { SignInButton, SignOutButton, currentUser } from "@clerk/nextjs";
 export default async function Home() {
   noStore();
   const user = await currentUser();
-
+  const { data } = await api.post.getAll().fetch();
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
